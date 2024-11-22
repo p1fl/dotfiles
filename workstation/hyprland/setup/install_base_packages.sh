10c2464e5de9dc34e0acf809267d9158151be9b2
p1fl <some.pifl@gmail.com>
2024-11-22 02:58:02 +0100

    wip feat: add installation script for base packages on arch hyprland

---
+++ workstation/hyprland/setup/install_base_packages.sh
@@ -0,0 +1,23 @@
+# vim: ts=2 sw=2:
+#/bin/bash
+
+#
+# All System Base Packages for a fresh Arch Install
+#
+
+BASE_PACKAGES=(
+	networkmanager
+	vim
+	git
+	curl
+	jq
+	pipewire-alsa
+	playerctl
+	pamixer
+	pavucontrol
+	bluez
+	bluez-utils
+	blueman
+)
+
+
